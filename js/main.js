0c90b398a965fdc6143daf20553686a5077508d1
Dimboba <lazarkovdmitry@yandex.ru>
2023-03-05 19:44:49 +0300

    Task2fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 function getRandomInteger (from, to) {
-  return  Math.floor(Math.random()*(to - from + 1) + from);
+  let low = Math.ceil(Math.min(from, to));
+  let high = Math.floor(Math.max(from, to));
+  return  Math.floor(Math.random()*(high - low + 1) + low);
 }
 
 function checkLength(str, maxLength = 0) {
@@ -7,12 +9,12 @@ function checkLength(str, maxLength = 0) {
   return false;
 }
 //Тестирование
-/*
+
 let s = '';
 
 for(let i = 0; i < 10; i++){
-  //console.log(getRandomInteger(-1, 1));
+  console.log(getRandomInteger(0.5, 2.7));
   s += i;
-  console.log(checkLength(s, 2));
+  //console.log(checkLength(s, 2));
 }
-*/
+
