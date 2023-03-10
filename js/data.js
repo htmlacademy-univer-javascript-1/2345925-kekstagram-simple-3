5c6dbb7a97f4e697c3b454cd85775383389813ac
Dimboba <lazarkovdmitry@yandex.ru>
2023-03-10 18:06:23 +0300

    fix10-03

--- js/data.js
+++ js/data.js
@@ -3,13 +3,13 @@ import {getRandomInteger} from './util.js'
 let photosInfo = [];
 
 for(let i = 0; i < 25; i++){
-  photosInfo[i] = {
+  photosInfo.push({
     id: i+1,
-    url: 'photos/' + (i+1) + '.jpg',
+    url: `photos/${i+1}.jpg`,
     description: 'Я - прекрасное описание картинки с id равным: ' + (i+1),
     likes: getRandomInteger(15, 200),
     comments: getRandomInteger(0, 200)
-  }
+  });
 }
 
 export {photosInfo};
